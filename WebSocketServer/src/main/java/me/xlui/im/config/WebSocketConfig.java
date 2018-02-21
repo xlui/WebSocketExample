7730cffea8752a9f670beba44c05c922765865f4
xlui <liuqi0315@gmail.com>
2018-02-21 20:29:55 +0800

    Add handshake interceptor

--- WebSocketServer/src/main/java/me/xlui/im/config/WebSocketConfig.java
+++ WebSocketServer/src/main/java/me/xlui/im/config/WebSocketConfig.java
@@ -11,10 +11,12 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 @EnableWebSocketMessageBroker
 public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 	// 注册 STOMP 协议的节点（Endpoint），并映射为指定的 URL
+	// 我们使用 STOMP，所以不需要再实现 WebSocketHandler。
+	// 实现 WebSocketHandler 的目的是接收和处理消息，STOMP 已经为我们做了这些。
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
 		// 注册 STOMP 协议的节点，并指定使用 SockJS 协议
-		stompEndpointRegistry.addEndpoint("/im").withSockJS();
+		stompEndpointRegistry.addEndpoint("/im").addInterceptors(new HandshakeInterceptor()).withSockJS();
 	}
 
 	// 配置使用消息代理
